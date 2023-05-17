abc55d82c100346fc4b2686fe8311188a577e0cc
FrancieleAires <f.aires2016@gmail.com>
2023-05-18 04:59:57 +0900

    update

---
+++ www/components/script.js
@@ -0,0 +1,20 @@
+let total = 0;
+
+function calcularAssento(valor) {
+  total += valor;
+  document.getElementById("valorTotal").innerText = total;
+}
+
+function limparValores() {
+  total = 0;
+  document.getElementById("valorTotal").innerText = total;
+}
+  
+  
+
+
+
+
+
+
+
